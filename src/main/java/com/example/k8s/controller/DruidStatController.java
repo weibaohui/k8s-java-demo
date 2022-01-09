b51b7fe289a8142ebccbfd8806f9912adc1f910f
weibaohui <weibaohui@yeah.net>
2022-01-09 21:10:37 +0800

    log

--- src/main/java/com/example/k8s/controller/DruidStatController.java
+++ src/main/java/com/example/k8s/controller/DruidStatController.java
@@ -1,6 +1,7 @@
 package com.example.k8s.controller;
 
 import com.alibaba.druid.stat.DruidStatManagerFacade;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,14 @@ import java.util.Map;
  * @author weibh
  */
 @RestController
+@Slf4j
 public class DruidStatController {
-    @GetMapping("/druid/stat")
+    @GetMapping("/druidx/stat")
     public List<Map<String, Object>> druidStat() {
-        // DruidStatManagerFacade#getDataSourceStatDataList 该方法可以获取所有数据源的监控数据，除此之外 DruidStatManagerFacade 还提供了一些其他方法，你可以按需选择使用。
+        log.debug("xxx");
+        log.info("xxx");
+        log.warn("xxx");
+        log.info("xxx");
         return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
     }
 }
